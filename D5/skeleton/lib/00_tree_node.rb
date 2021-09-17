6bfa199d50ec2119563ed93c36556f7e5db71bd8
Alan Chiu <ms0372631@gmail.com>
2021-09-17 15:22:20 -0700

    created initialize method and parent method

--- D5/skeleton/lib/00_tree_node.rb
+++ D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,15 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent=(parent)
+        @parent = parent
+        parent.children << self
+    end
 end
