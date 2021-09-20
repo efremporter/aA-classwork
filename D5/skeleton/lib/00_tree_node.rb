03eba4606e673ce35d0ec20675ec6d56985e152f
Efrem <eopiscool@gmail.com>
2021-09-20 10:39:19 -0700

    Fix parent= method

--- D5/skeleton/lib/00_tree_node.rb
+++ D5/skeleton/lib/00_tree_node.rb
@@ -1,15 +1,26 @@
 class PolyTreeNode
+  attr_reader :parent, :children, :value
 
-    attr_reader :parent, :children, :value
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
 
-    def initialize(value)
-        @parent = nil
-        @children = []
-        @value = value
+  def parent=(new_parent)
+    if !parent.nil?
+      
     end
-
-    def parent=(parent)
-        @parent = parent
-        parent.children << self
+    if !new_parent.nil? && !new_parent.children.include?(self) 
+      new_parent.children << self 
     end
+    @parent = new_parent
+  end
 end
+
+# node1 = PolyTreeNode.new(1)
+# node1.parent = node2
+
+# arr = [1, 2, 3, 4]
+# Array.new([1, 2, 3, 4])
+
