ebc06153b7f61e81d39749e63f33ff91cc388780
Alan Chiu <ms0372631@gmail.com>
2021-09-20 15:23:04 -0700

    valid moves

--- D5/knights_travails/knight_path_finder.rb
+++ D5/knights_travails/knight_path_finder.rb
@@ -1,11 +1,31 @@
 require_relative "poly_node_tree.rb"
 class KnightPathFinder
 
-  def initialize(arr)
-    @root_node = arr
-    @directions = [[1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1], [2, -1], [-2, -1]]
+  attr_reader :root_node, :directions
+
+  def initialize(pos)
+    @root_node = pos
+    @directions = []
+  end
+
+  def self.valid_moves(pos)
+    i = 0
+    possible_x = [-2, -1, 1, 2]
+    possible_y = [-2, -1, 1, 2]
+   
+    possible_x.each_with_index do |x, index1|
+      possible_y.each_with_index do |y, index2|
+        if index2 >= index1 
+         if (x + pos[0] <= 8 && x + pos[0] >= 0) && (y + pos[1] <= 8 && y + pos[1] >= 0)
+            directions << [x + pos[0], y + pos[1]]
+         end
+        end
+      end
+    end
+    
+
   end
 
 end
 
-
+# [[1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1], [2, -1], [-2, -1]]
