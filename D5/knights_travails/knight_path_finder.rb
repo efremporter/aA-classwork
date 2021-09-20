d7050a2569700acc8f776b375bc03f37169b7cf5
Alan Chiu <ms0372631@gmail.com>
2021-09-20 16:30:40 -0700

    phase 2

--- D5/knights_travails/knight_path_finder.rb
+++ D5/knights_travails/knight_path_finder.rb
@@ -2,23 +2,30 @@ require_relative "poly_node_tree.rb"
 require "byebug"
 class KnightPathFinder
 
-  attr_reader :root_node, :directions
+  attr_reader :root_node
+  attr_accessor :considered_positions
 
   def initialize(pos)
     @root_node = pos
-    @directions = []
+    @considered_positions = []
   end
 
   def self.valid_moves(pos)
-    @considered_positions = []
+    @valid_positions = []
     i = 0
     possible_positions = [[1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1], [2, -1], [-2, -1]]
     possible_positions.each do |possible_pos|
       if (pos[0] + possible_pos[0] < 8 && pos[0] + possible_pos[0] >= 0) && (pos[1] + possible_pos[1] < 8 && pos[1] + possible_pos[1] >= 0)
-        @considered_positions << [possible_pos[0] + pos[0], possible_pos[1] + pos[1]]
+        @valid_positions << [possible_pos[0] + pos[0], possible_pos[1] + pos[1]]
       end  
     end
-    @considered_positions
+    @valid_positions
   end
+
+  def new_move_positions(pos)
+    considered_positions.concat(KnightPathFinder.valid_moves(pos))
+  end
+
+
 
 end
