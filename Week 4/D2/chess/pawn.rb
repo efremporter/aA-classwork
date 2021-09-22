c1e778f0769c0bfe4c6828df98758bca58feab79
ecbell <ebell691@gmail.com>
2021-09-22 14:21:13 -0500

    Populate board

--- Week 4/D2/chess/pawn.rb
+++ Week 4/D2/chess/pawn.rb
@@ -1,8 +1,10 @@
+require_relative "piece.rb"
+
 class Pawn < Piece
 
   def initialize(color, board, pos) 
     super(color, board, pos)
-
+    @symbol = self.symbol
   end
 
   def symbol
--- Week 4/D2/chess/piece.rb
+++ Week 4/D2/chess/piece.rb
@@ -3,8 +3,6 @@ class Piece
   attr_reader :board, :color, :pos
   attr_writer :pos
 
-  SYM = [:K, :Q, :B, :R, :H, :P]
-
   def initialize(color, board, pos)
     @board = board
     @color = color
@@ -13,7 +11,7 @@ class Piece
 
 
   def empty?
-    return true if board[pos] == nil
+    return true if board[pos] == "Nullpiece"
     false
   end
 
@@ -21,4 +19,9 @@ class Piece
     x, y = pos
     board[x][y] = val
   end
+
+  # def move_into_check?(end_pos)
+  #   #need to know if piece has checked the king with the end_pos.
+  # end
+  
 end
