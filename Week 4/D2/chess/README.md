de4ce21534e2918619451418d354f725b60baf05
ecbell <ebell691@gmail.com>
2021-09-22 12:40:07 -0500

    Populate board

---
+++ Week 4/D2/chess/README.md
@@ -0,0 +1,32 @@
+Pseudo code for Chess:
+
+BOARD:
+1. populate the board in the board class.
+2. Pieces need to be aware of where they are on the board and vis versa.
+    i.e. figure out a way to get the pieces and board to communicate. 
+    This could be via the pos method on the board.
+3. In the board class, the board is only concerned whether the space is empty. It's valid to the board
+4. SYM: --> :K, :Q, :R, :H, :P, :B
+
+
+4. check: 
+    - analyze who on the opponent's side can MOVE to our king's position. 
+    - if no opponent's piece can move to King's position, not check.
+        - opponent.piece.bfs = king.position
+        - can we get out of it? --> NEED LOGIC HERE. -->  
+    - if so, check. 
+    - 
+
+5. check_mate:
+    analyze each piece on the board.
+
+
+PIECE
+1. valid move for the piece is concerned with how the piece can move and if there is a piece in that position.
+2. Piece moves: similar to the board moves. takes a start and end position. just needs to check if it's a valid move
+    i.e. check if piece can move in that way and check if there are any pieces IN its path
+    AND check if there's a piece in the end_pos.
+
+
+GAME:
+1. 
--- Week 4/D2/chess/board.rb
+++ Week 4/D2/chess/board.rb
@@ -1,12 +1,34 @@
 class Board
 
+    attr_accessor :grid
+    def populate
+        #[]([0,0]) => [[0,0]] => [0][0]
+        grid[[0,0]] = "BR1"
+        # grid[[0,7]] = "BR2"
+        # grid[[0,1]] = "BH1"
+        # grid[[0,6]] = "BH2"
+        # grid[[0,2]] = "BB1"
+        # grid[[0,5]] = "BB2"
+        # grid[[0,3]] = "BQ"
+        # grid[[0,4]] = "BK"
+        # grid[[7,0]] = "WR1"
+        # grid[[7,7]] = "WR2"
+        # grid[[7,1]] = "WH1"
+        # grid[[7,6]] = "WH2"
+        # grid[[7,2]] = "WB1"
+        # grid[[7,5]] = "WB2"
+        # grid[[7,3]] = "WQ"
+        # grid[[7,4]] = "WK"
+    end
+    
     def initialize
-        @board = Array.new(8) {Array.new(8)}
+        @grid = Array.new(8) {Array.new(8)}
+        
     end
 
-    def [](pos)
+    def [](pos) 
         x,y = pos
-        return board[x][y]
+        return grid[x][y] 
     end
     
     def move_piece(start_pos, end_pos)
@@ -18,7 +40,10 @@ class Board
     end
 
     def []=(pos, val)
-      board[pos] = val
+        x, y = pos
+        grid[x][y] = val
+
+        #grid[position[0]][position[1]] = value
     end
 
     def valid_pos?(pos)
@@ -30,7 +55,18 @@ class Board
     end
 
     def add_piece(piece, pos)
-      board[pos] = piece
+      grid[pos] = piece
     end
 
+    
+
+    def print
+        grid.each do |row|
+            p row
+        end
+    end
 end 
+
+b1 = Board.new
+ p b1.populate
+b1.print
