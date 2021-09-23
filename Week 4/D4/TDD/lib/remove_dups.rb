c6883166bbf3fd67293e46d45f28b1c141b8d504
pamajama <pamela.palomita@gmail.com>
2021-09-23 15:11:19 -0700

    create my_transpose

--- Week 4/D4/TDD/lib/remove_dups.rb
+++ Week 4/D4/TDD/lib/remove_dups.rb
@@ -1,19 +1,30 @@
 
-def remove_dups(arr)
+def remove_dups(arr) 
     arr.uniq
 end
 
 class Array
-
-  def two_sum(array)
+  def two_sum
     zero_sum = []
-    (0...array.length - 1).each do |i|
-      (i + 1...array.length).each do |j|
-        zero_sum << [i, j] if array[i] + array[j] == 0
+    (0...self.length - 1).each do |i|
+      (i + 1...self.length).each do |j|
+        zero_sum << [i, j] if self[i] + self[j] == 0
       end
     end
     return zero_sum
   end
-  
+end
+
+
+def my_transpose(two_d_array)
+  arr = []
+  (0...two_d_array.length).each do |i|
+    holder = []
+    (0...two_d_array.length).each do |j|
+      holder << two_d_array[j][i]
+    end
+    arr << holder
+  end
+  arr
 end
 
--- Week 4/D4/TDD/spec/remove_dups_spec.rb
+++ Week 4/D4/TDD/spec/remove_dups_spec.rb
@@ -1,20 +1,43 @@
 require "remove_dups.rb"
-
+ 
 describe "#remove_dups" do
   it "removes duplicate elements" do
     expect(remove_dups([1, 1, 1, 2, 3])).to eq([1,2,3])
   end
 end
 
-describe "Array" do
-  subject(:array) { Array.new([-1, 0, 2, -2, 1]) }
-  describe "#two_sum" do
+describe Array do
+  # subject(:array) { Array.new([-1, 0, 2, -2, 1]) }
+  describe "Array#two_sum" do
     # it "receives an array" do
     #   expect(two_sum).to receive_a(Array)
     # end
     
     it "finds all indices of opposite pairs" do
-      expect(two_sum(array)).to eq([[0, 4],[2, 3]])
+      expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4],[2, 3]])
     end
   end
+end
+
+describe "#my_transpose" do 
+  subject(:arr1) { Array.new([
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ]) }
+
+  it "takes in a 2D array" do
+    expect { my_transpose(arr1) }.to_not raise_error
+  end
+
+  it "converts a row oriented array to a column oriented array" do
+    expect(my_transpose(arr1)).to eq(cols = [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]
+    ])
+    
+
+  end
+
 end
