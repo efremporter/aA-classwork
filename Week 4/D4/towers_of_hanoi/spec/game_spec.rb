50b47b8b4fad17fd119aed6145a64f80f3c2dd09
Efrem <eopiscool@gmail.com>
2021-09-23 16:13:01 -0700

    Initialize method

---
+++ Week 4/D4/towers_of_hanoi/spec/game_spec.rb
@@ -0,0 +1,14 @@
+require "game.rb"
+
+describe Game do 
+  subject(:game) { Game.new }
+  describe "#initialize" do 
+    it "sets @grid to empty 2D array" do
+
+    end
+
+    it "sets game_over as false" do
+
+    end
+  end
+end
