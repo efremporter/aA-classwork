c0393c1a4bb2fb2f717032dd4caa8b2fa5dbe411
pamajama <pamela.palomita@gmail.com>
2021-09-23 16:29:36 -0700

    add specs for init

--- Week 4/D4/towers_of_hanoi/spec/game_spec.rb
+++ Week 4/D4/towers_of_hanoi/spec/game_spec.rb
@@ -1,14 +1,20 @@
-require "game.rb"
+require "game.rb" 
 
 describe Game do 
-  subject(:game) { Game.new }
+  subject(:game) { Game.new(5) }
+  subject(:array) { [[5,4,3,2,1],[],[]] }
+  subject(:game_over) {false}
   describe "#initialize" do 
-    it "sets @grid to empty 2D array" do
+    it "takes in a number to represent the number of stacks" do 
+      expect { Game.new(5) }.to_not raise_error
+    end
 
+    it "sets @grid to empty 2D array" do
+      expect(game).to eq(array)
     end
 
     it "sets game_over as false" do
-
+      expect(game_over).to be(false)
     end
   end
 end
