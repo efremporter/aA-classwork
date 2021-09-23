df08ea60066dc0ab87c0e14a54d9e86584808391
Efrem <eopiscool@gmail.com>
2021-09-23 14:31:16 -0700

    Working on two_sum method

--- Week 4/D4/TDD/lib/remove_dups.rb
+++ Week 4/D4/TDD/lib/remove_dups.rb
@@ -1,3 +1,19 @@
+
 def remove_dups(arr)
     arr.uniq
 end
+
+class Array
+
+  def two_sum(array)
+    zero_sum = []
+    (0...array.length - 1).each do |i|
+      (i + 1...array.length).each do |j|
+        zero_sum << [i, j] if array[i] + array[j] == 0
+      end
+    end
+    return zero_sum
+  end
+  
+end
+
--- Week 4/D4/TDD/spec/remove_dups_spec.rb
+++ Week 4/D4/TDD/spec/remove_dups_spec.rb
@@ -1,6 +1,20 @@
+require "remove_dups.rb"
+
 describe "#remove_dups" do
-  it "remove duplicate elements" do
-    expect(remove_dups([1,1,1,2,3])).to eq([1,2,3])
+  it "removes duplicate elements" do
+    expect(remove_dups([1, 1, 1, 2, 3])).to eq([1,2,3])
   end
 end
 
+describe "Array" do
+  subject(:array) { Array.new([-1, 0, 2, -2, 1]) }
+  describe "#two_sum" do
+    # it "receives an array" do
+    #   expect(two_sum).to receive_a(Array)
+    # end
+    
+    it "finds all indices of opposite pairs" do
+      expect(two_sum(array)).to eq([[0, 4],[2, 3]])
+    end
+  end
+end
