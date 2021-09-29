3e2b9dbb1b6fbaf7c2033f64fcbdff3dac8dbb1e
Matthew Lese <matthewjlese@gmail.com>
2021-09-29 10:56:28 -0700

    completes question_likes table, begins inserting values

--- W5/D3/import_db.sql
+++ W5/D3/import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   fname TEXT NOT NULL,
@@ -30,3 +32,32 @@ CREATE TABLE replies (
   FOREIGN KEY(subject_question) REFERENCES questions(id),
   FOREIGN KEY(replying_user) REFERENCES users(id)
 );
+
+CREATE TABLE question_likes (
+  users_id INTEGER NOT NULL,
+  questions_id INTEGER NOT NULL,
+  FOREIGN KEY(users_id) REFERENCES users_id(id),
+  FOREIGN KEY(questions_id) REFERENCES questions(id)
+);
+
+INSERT INTO
+  users (first_name, last_name)
+VALUES
+  (Matt, Lese),
+  (Efrem, Porter);
+
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('ruby programming', 'how do we code using ruby?', SELECT id FROM users WHERE fname = 'Efrem' AND lname = 'Porter')
+  ('sql programming', 'what companies utilize SQL?', SELECT id FROM users WHERE fname = 'Matt' AND lname = 'Lese')
+
+INSERT INTO
+  question_follows (user_id, question_id)
+VALUES
+  (SELECT id FROM users WHERE fname = 'Efrem' AND lname = 'Porter', SELECT id FROM questions WHERE author_id = (SELECT id FROM users WHERE fname = 'Matt' AND lname = 'Lese'))
+
+INSERT INTO
+  replies (body, subject_question, replying_user)
+VALUES
+  ('I think Apple is one.', SELECT id FROM questions WHERE)
