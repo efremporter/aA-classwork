8fb756b98c6b927b29bc44f5d7fc6e9a82adb85c
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-10-01 15:01:03 -0700

    Complete 01_queries.rb

--- W5/D5/skeleton/movie_buff/01_queries.rb
+++ W5/D5/skeleton/movie_buff/01_queries.rb
@@ -44,7 +44,7 @@ def biggest_cast
   # largest casts (i.e most actors)
 
   Movie
-    .select("movies.id, title, COUNT(actor_id)")
+    .select("movies.id, title")
     .joins(:castings)
     .group("movies.title, movies.id")
     .order('COUNT(castings.actor_id) DESC')
@@ -56,13 +56,19 @@ def directed_by_one_of(them)
   #
   # Movie.where('yr IN (?)', years)
   #
-  # We can use IN to test if an element is present in an array.
+  # We can use IN to test if an element is present in an array. YOU MUST WRAP ? WITH THE PARENTHESIS
   #
   # ActiveRecord gives us an even better way to write this:
   #
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
+
+  Movie
+    .joins(:director)
+    .where('actors.name IN (?)', them)
+    .select(:id, :title)
+
 
 end
 
@@ -77,5 +83,7 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+
+  Movie.where('yr < 1940').pluck(:title)
 
 end
