ce153e3cdceb6b15dbed593b0d862cec343233ad
jacky <jc6825@stern.nyu.edu>
2021-10-06 19:12:12 -0400

    Create project and copy:

---
+++ W6/D3/RoutesAgain/app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+
+  def index 
+    render plain: "I'm in the index action!" 
+    # users = User.all 
+    # render json: users
+  end
+
+  def show 
+    # user = User.find(params[:id])
+    # render json: 'this is a test' 
+    render json: params
+  end
+  
+  def create
+  #   user = User.new(user_params)
+
+  #   if user
+  #      redirect_to "/users/#{user.id}"
+  #   else
+      
+  #   end
+    render json: params
+  end 
+
+  def user_params 
+    params.require(:user).permit(:username, :email, :age, :political_affliation)
+  end 
+end 
---
+++ W6/D3/RoutesAgain/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  get '/users/new', to: 'users#new'
+  post '/users', to: 'users#create'
+  get '/users/:id/edit', to: 'users#edit'
+  patch '/users/:id', to: 'users#update' 
+  delete '/users', to: 'users#destroy' 
+end
