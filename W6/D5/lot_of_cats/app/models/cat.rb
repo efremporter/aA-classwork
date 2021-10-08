087fed54dbbc26fb8c987d1f09d8717a7e495aa4
tharpercp <tharpercp@gmail.com>
2021-10-08 15:08:11 -0700

    working on controller methods

--- W6/D5/lot_of_cats/app/models/cat.rb
+++ W6/D5/lot_of_cats/app/models/cat.rb
@@ -1,3 +1,4 @@
 class Cat < ApplicationRecord
-  validates_inclusion_of :color, :in ["black", "cinnamon", "blue", "lavender", "carmel", "amber", "cream"]
+  validates_inclusion_of :cats, :color, :in ["black", "cinnamon", "blue", "lavender", "carmel", "amber", "cream"]
+  validates_inclusion_of :cats, :sex, :in ["M", "F"]
 end
