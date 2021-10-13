15c21c33ca49fa6bb71bb1441e472c9aa526bdf2
Efrem <eopiscool@gmail.com>
2021-10-13 16:02:49 -0700

    Working on Routes

--- W7/D3/Goals/goal_project/spec/controllers/users_controller_spec.rb
+++ W7/D3/Goals/goal_project/spec/controllers/users_controller_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to be_success
     end
   end
+
+  describe "GET #show" do
+    it "renders the show template" do
+      User.create!(username: "username", password: "password")
+      get :show, id: 1
+      expect(response).to render_template(:show)
+    end
+
+    context "if the user does not exist" do 
+      it "is not a success" do
+        begin
+          get :show, id: -1
+        rescue
+          ActiveRecord::RecordNotFound
+        end
+        expect(response).not_to render_template(:show)
+      end
+    end
+  end
 end
