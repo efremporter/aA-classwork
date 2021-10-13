168360d4c5b3c7115de2fc257f893e2c888ad6b0
Efrem <eopiscool@gmail.com>
2021-10-13 14:20:08 -0700

    Started User Specs

---
+++ W7/D3/Goals/goal_project/spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "Validations" do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+    it { should validate_uniqueness_of(:username) }
+  end 
+end
