b04160862b32f8367adb6b9e40b1df39cdcbed16
Mark <mrkdawes8@gmail.com>
2021-10-13 19:46:24 -0400

    add user controller specs

--- W7/D3/Goals/goal_project/spec/controllers/users_controller_spec.rb
+++ W7/D3/Goals/goal_project/spec/controllers/users_controller_spec.rb
@@ -27,4 +27,28 @@ RSpec.describe UsersController, type: :controller do
       end
     end
   end
+
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new
+      expect(response).to be_success
+    end
+  end
+
+  describe "POST #create" do
+    context "with invalid params" do
+      it "renders the new template" do
+        post :create, user: { username: "bob43" }
+        expect(response).to render_template(:new)
+      end
+    end
+
+    context "with valid params" do
+      it "redirect to user page on success" do
+        post :create, user: { username: "stev56", password: "password45" }
+        expect(response).to redirect_to(user_url(User.find_by(username: 'stev56')))
+      end
+    end
+  end
+
 end
