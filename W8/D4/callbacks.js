96bba5b49080f0d3d6630ce538ef74a0888fbd1c
Cal Brown <calsbrown4@gmail.com>
2021-10-21 14:53:24 -0700

    Printtime

--- W8/D4/callbacks.js
+++ W8/D4/callbacks.js
@@ -1,6 +1,4 @@
-// function setInterval(cb[, delay[, ...args[]]]) {
 
-// }
 class Clock {
   constructor(hours, minutes, seconds) {
     this.hours = hours;
@@ -9,7 +7,24 @@ class Clock {
     this.printTime();
   }
   printTime() {
-    console.log(`${this.hours}:${this.minutes}:${this.seconds}`)
-    // console.log('hi');
+  let h;
+  let s;
+  let m;
+      if (this.hours < 10){
+       h =`0${this.hours}:`
+      } else{
+      h= `${this.hours}:`
+      }
+      if (this.minutes < 10) {
+      m =`0${this.minutes}:`
+      } else {
+      m = `${this.minutes}:`
+      }
+      if (this.seconds < 10) {
+      s = `0${this.seconds}`
+      } else {
+        s = `${this.seconds}`
+      }
+      console.log(h + m + s);
   }
 }
