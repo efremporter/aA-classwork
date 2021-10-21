94672f111a8a13325ef5a0301cb7e0f34cae1c19
Efrem <eopiscool@gmail.com>
2021-10-21 16:03:04 -0700

    finished printTime and _tick

--- W8/D4/callbacks.js
+++ W8/D4/callbacks.js
@@ -1,30 +1,56 @@
-
 class Clock {
   constructor(hours, minutes, seconds) {
     this.hours = hours;
     this.minutes = minutes;
     this.seconds = seconds;
     this.printTime();
+
+    let context = this;
+    setInterval(this._tick.bind(context), 1000);  
   }
   printTime() {
   let h;
   let s;
   let m;
-      if (this.hours < 10){
-       h =`0${this.hours}:`
-      } else{
-      h= `${this.hours}:`
-      }
-      if (this.minutes < 10) {
-      m =`0${this.minutes}:`
+
+      if (this.hours < 10) {
+        h = `0${this.hours}:`
       } else {
-      m = `${this.minutes}:`
+        h = `${this.hours}:`
       }
+
+      if (this.minutes < 10) {
+        m = `0${this.minutes}:`
+      } else {
+        m = `${this.minutes}:`
+      }
+
       if (this.seconds < 10) {
-      s = `0${this.seconds}`
+        s = `0${this.seconds}`
       } else {
         s = `${this.seconds}`
       }
       console.log(h + m + s);
+  }
+
+  _tick() {
+
+    if (this.seconds < 60){
+      this.seconds += 1;
+    } else {
+      this.seconds = 0;
+      this.minutes += 1
+    }
+
+    if (this.minutes === 60) {
+      this.minutes = 0;
+      this.hours += 1;
+    }
+
+    if (this.hours === 24) {
+      this.hours = 0
+    }
+
+    this.printTime();
   }
 }
