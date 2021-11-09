34c8bc66854a53870c6ab41f1414b1981a2a7511
Efrem <eopiscool@gmail.com>
2021-11-09 14:19:17 -0800

    Finished phase 1

---
+++ W11/D2/todo_list/frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let main = getElementById("main");
+  ReactDOM.render(<h1>Todos App</h1>, main)
+})
