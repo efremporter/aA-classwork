12ec71eb6f13c90dda95d62c305a68546ef20bdb
Efrem <eopiscool@gmail.com>
2021-11-10 13:55:09 -0800

    Finished phase 1

--- W11/D2/todo_list/frontend/todo_redux.jsx
+++ W11/D2/todo_list/frontend/todo_redux.jsx
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./components/root"
 import configureStore from './store/store';
 import { receiveTodos, receiveTodo, removeTodo } from './actions/todo_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("hello")
-
+  const main = document.getElementById("main");
   const store = configureStore();
-
+  ReactDOM.render(<Root store={store}/>, main)
   window.store = store
   window.receiveTodos = receiveTodos
   window.receiveTodo = receiveTodo
