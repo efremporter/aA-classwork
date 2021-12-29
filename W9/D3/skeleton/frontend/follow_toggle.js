ec30fa6bc5ec677f5ae6e8f297fea43a684a731c
Kirti Harode <kikihrd20@gmail.com>
2021-12-29 14:48:22 -0800

    got user id and follow status

---
+++ W9/D3/skeleton/frontend/follow_toggle.js
@@ -0,0 +1,8 @@
+class FollowToggle{
+    constructor(){
+        this.el = $("button.follow-toggle");
+
+        this.followState = x.dataset[initial-follow-state];
+        this.userId = x.dataset[user-id];
+    }
+}
